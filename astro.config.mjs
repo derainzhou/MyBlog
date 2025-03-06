885359f6836aaff54dc6b7cbd061780ae6b3cdb5
DerainZhou <derainzhou@gmail.com>
2025-03-06 14:29:54 +0800

    完整新手教程

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-    site: "https://derainblog.netlify.app"
+  site: "https://derainblog.netlify.app",
+  integrations: [preact()]
 });
